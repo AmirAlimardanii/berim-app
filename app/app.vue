1369021fddf4e844c3a243bba9f42196e15c464c
AmirAlimardani <amiralimardani.dev@gmail.com>
2025-09-19 21:00:25 +0330

    `feat: update dependencies and plugins for Nuxt.js project`

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,16 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <div class="font-bold bg-red-600">
+      test for tailwind
+    </div>
+    <button class="btn btn-accent">
+      Accent
+    </button>
+
+    <Icon
+      name="tabler:map"
+      style="color: white"
+      size="32"
+    />
   </div>
 </template>
