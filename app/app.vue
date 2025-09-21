00b006bda72eb0e25acf354a6dad8d68206d95fc
AmirAlimardani <amiralimardani.dev@gmail.com>
2025-09-21 21:25:12 +0330

    create sample header and hero section

--- app/app.vue
+++ app/app.vue
@@ -1,16 +1,5 @@
 <template>
-  <div>
-    <div class="font-bold bg-red-600">
-      test for tailwind
-    </div>
-    <button class="btn btn-accent">
-      Accent
-    </button>
-
-    <Icon
-      name="tabler:map"
-      style="color: white"
-      size="32"
-    />
-  </div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
---
+++ app/components/app/nav-bar.vue
@@ -0,0 +1,11 @@
+<template>
+<div class="navbar bg-primary text-primary-content fixed top-0 z-50">
+    <div class="navbar-start">
+  <NuxtLink to="/" class="btn btn-ghost text-xl">Berim App</NuxtLink>
+  </div>
+  <div class="navbar-end">
+    <button class="btn">SIGN IN <Icon name="tabler:login-2" size="22" /></button>
+
+  </div>
+</div>
+</template>
