6f8df8f57463e0241710d99723bc9189df872076
AmirAlimardani <amiralimardani.dev@gmail.com>
2025-09-24 00:35:07 +0330

    add databse migrations and location table

---
+++ app/lib/db/schema/location.ts
@@ -0,0 +1,9 @@
+import { int, sqliteTable, text } from "drizzle-orm/sqlite-core";
+
+export const locations = sqliteTable("locations", {
+  id: int().primaryKey({ autoIncrement: true }),
+  name: text().notNull(),
+  slug: text().notNull(),
+  description: text().notNull(),
+ 
+});
