a1ec7eb3d6000ed917767fbcb30bf38f86b22485
AmirAlimardani <amiralimardani.dev@gmail.com>
2025-09-23 22:37:55 +0330

    add zod env schema validation

---
+++ app/lib/env.ts
@@ -0,0 +1,10 @@
+import * as z from "zod"; 
+
+
+const EnvScheme = z.object({
+    NODE_ENV: z.string(),
+})
+
+export type EnvScheme = z.infer<typeof EnvScheme>;
+
+export default EnvScheme.parse(process.env)
