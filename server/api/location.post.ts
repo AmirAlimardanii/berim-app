f38b56afc51bb84f01162a8d27163fb9aabeef2a
AmirAlimardani <amiralimardani.dev@gmail.com>
2025-10-13 23:01:01 +0330

    insert into db

--- server/api/location.post.ts
+++ server/api/location.post.ts
@@ -1,6 +1,18 @@
-import { InsertLocation } from '~/lib/db/schema'
+import db from '~/lib/db'
+import { InsertLocation, location } from '~/lib/db/schema'
 
 export default defineEventHandler(async (event) => {
+
+  if(!event.context.user){
+
+    return sendError(event, createError({
+      statusCode: 403,
+      statusMessage: 'Un Authorized'
+    }))
+
+  }
+
+
   const result = await readValidatedBody(event, InsertLocation.safeParse)
 
   if (!result.success){
@@ -9,5 +21,12 @@ export default defineEventHandler(async (event) => {
         statusMessage: 'Invalid Location Data'
       }))
   }
-  return result.data
+
+ const [created] = await db.insert(location).values({
+    ...result.data,
+    slug: result.data.name.replaceAll(' ', '-').toLowerCase(),
+    userId: event.context.user.id
+  }).returning();
+
+  return created;
 })
